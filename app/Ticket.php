bbfd9aa4988c26a0a30a98926f34f5423ec58234
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-12-12 15:32:36 -0500

    Feature: Added user to ticket relationship such that a user can have many tickets and a ticket can belong to a user

--- app/Ticket.php
+++ app/Ticket.php
@@ -20,4 +20,9 @@ class Ticket extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
