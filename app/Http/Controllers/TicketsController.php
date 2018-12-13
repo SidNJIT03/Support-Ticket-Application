4117566e3f25425be97b6e410e4e1c6879a46ac2
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-12-12 19:05:05 -0500

    Feature: Added an index function that gets all the tickets that have been created and a close function to TicketsController which on closure of tickets sends notification to the user through mailer

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -65,4 +65,27 @@ class TicketsController extends Controller
         return view('tickets.show', compact('ticket', 'category', 'comments'));
     }
 
+    public function index()
+    {
+        $tickets = Ticket::paginate(10);
+        $categories = Category::all();
+
+        return view('tickets.index', compact('tickets', 'categories'));
+    }
+
+    public function close($ticket_id, AppMailer $mailer)
+    {
+        $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+
+        $ticket->status = 'Closed';
+
+        $ticket->save();
+
+        $ticketOwner = $ticket->user;
+
+        $mailer->sendTicketStatusNotification($ticketOwner, $ticket);
+
+        return redirect()->back()->with("status", "The ticket has been closed.");
+    }
+
 }
